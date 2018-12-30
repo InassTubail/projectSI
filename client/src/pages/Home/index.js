e1eede56a662e7fb091d364a943460fe3ed25c15
InassTubail <anoos.hanii@gmail.com>
2018-12-30 11:58:46 +0200

    File strucure

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="">
+    <p>Welcome home</p>
+  </div>
+);
+
+export default Home;
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+app.get('/api/v1/news', (req, res, next) => {
+  const item = {
+    id: 1,
+    main_headline: 'Here is the news',
+    details: {
+      blurb: 'Loads of news Loads of news Loads of news Loads of news Loads of news Loads of news',
+      more_details: 'info.txt'
+    }
+  }
+  news = { posts: [item, item, item, item] }
+  return res.json(news)
+})
+
+app.listen(process.env.PORT || 9000);
