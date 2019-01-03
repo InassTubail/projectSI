a5c38da9336983747f69b634441cc8e9928d776b
InassTubail <anoos.hanii@gmail.com>
2019-01-03 11:27:40 +0200

    add git ignore

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <Switch>
-            <Route exact path="/" render={Home} />
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Router>
+      <Switch>
+        <Route exact path="/" render={Home} />
+      </Switch>
+    </Router>
+  </div>
+);
 
 export default App;
