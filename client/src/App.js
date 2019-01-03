7f55a52395ee633b7c449c55320523f0b17055d0
InassTubail <anoos.hanii@gmail.com>
2019-01-03 10:52:27 +0200

    make pure function

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Home from './pages/Home';
+import Home from './Pages/Home';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <Switch>
-            <Route exact path="/" render={Home} />
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Router>
+      <Switch>
+        <Route exact path="/" render={Home} />
+      </Switch>
+    </Router>
+  </div>
+);
 
 export default App;
